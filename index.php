2b8e1b4ed7985d8b250adf1f53af32b81b8f1129
wataru <wataru@hyuga.jp>
2021-09-30 10:28:38 +0900

    init.

---
+++ index.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>深見眼科</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="format-detection" content="telephone=no">
+<meta name="description" content="">
+<link rel="stylesheet" href="./js/slick/slick.css">
+<link rel="stylesheet" href="./js/slick/slick-theme.css">
+<link rel="stylesheet" href="./css/loaders.min.css">
+<link rel="stylesheet" href="./css/font.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/common.css?<?= time()?>">
+<link rel="stylesheet" href="./css/top.css">
+<link rel="stylesheet" href="./css/sub.css">
+<link rel="stylesheet" href="./css/contents.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+</head>
+<body>
+
+<div id="page">
+
+<header class="header">
+	<div class="inner">
+		<h1 class="logo"><a href="./index.html"><img src="./images/logo_top.svg" alt="深見眼科"></a></h1>
+		<div class="h_tel">tel.0565-34-1155</div>
+	</div>
+</header>
+
+<nav class="nav mincho">
+	<div class="nav_list">
+		<a href="./goaisatsu.html">院長ご挨拶</a>
+		<a href="./shinryou.html">診療について</a>
+		<a href="./ishi.html">医師紹介</a>
+		<a href="./shisetsu.html">施設紹介</a>
+		<a href="./rsvapp">診療予約</a>
+		<a href="./ortho.html">オルソケラトロジー<br>（自費診療）</a>
+		<a href="./myopine.html">マイオピン点眼薬<br>（自費診療）</a>
+		<a href="./recruit.html">採用情報</a>
+	</div>
+</nav>
+
+<header class="sub_header fix mincho">
+	<div class="inner">
+		<div class="sub_logo"><h1><a href="./index.php"><img src="./images/logo.svg" alt="深見眼科" width="178"></a></h1></div>
+		<div class="sp_tel"><a href="tel:0565-34-1155"><i class="icon-phone-circled"></i></a></div>
+		<div class="nav_wrap">
+			<div class="h_tel">tel.0565-34-1155</div>
+			<nav class="sub_nav no_active"></nav>
+		</div>
+	</div>
+	<div class="nav-button-wrap">
+		<div class="nav-button">
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+		<strong>MENU</strong>
+	</div>
+</header>
+
+<div class="main clearfix fix_pos">
+	<div class="slider">
+		<div class="mi mi1"><img src="./images/mes1.svg" alt="よく見える喜びと"></div>
+		<div class="mi mi2"><img src="./images/mes2.svg" alt="毎日の笑顔を"></div>
+		<div class="mi mi3"><img src="./images/mes3.svg" alt="皆さまにお届けしたい。"></div>
+	</div>
+	<div class="loader"><div class="line-spin-fade-loader"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
+</div>
+
+<div class="top_contents">
+
+<!--オルソケラトロジー-->
+<section class="top_ortho">
+	<div class="ortho">
+		<h2 class="mincho">オルソケラトロジーのご紹介</h2>
+		<div class="ortho_top">
+			<div class="ortho_desc clearfix"><img src="./images/moonstar.png" width="87">オルソケラトロジーとは特殊なコンタクトレンズを寝ている間に装用する事で近視の矯正を行う治療法の一種です。特に小児期の近視の進行抑制に効果があると注目されています。</div>
+			<a href="./ortho.html" class="b_ortho">詳しくはこちら<i class="icon-right-circle"></i></a>
+		</div>
+		<div class="ortho_bottom">
+			<h3 class="mincho">スペシャルコラム</h3>
+			<ul class="tile a1-b1-c2 am15">
+				<li><a href="./ortho_about.html" class="ortho_menu_button"><span>オルソケラトロジーとは</span></a></li>
+				<li><a href="./ortho_merit-demerit.html" class="ortho_menu_button"><span>オルソケラトロジーの向き不向き<br>メリットデメリット</span></a></li>
+				<li><a href="./ortho_cost.html" class="ortho_menu_button"><span>オルソケラトロジーの費用</span></a></li>
+				<li><a href="./ortho_qa.html" class="ortho_menu_button"><span>オルソケラトロジーQ&amp;A</span></a></li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<!--お知らせ-->
+<section class="top_news">
+	<div class="top_news_box">
+		<div>
+			<h2 class="t_top"><img src="./images/t_news.png" alt="お知らせ" width="126px"></h2>
+		</div>
+		<div>
+			<ul class="list_topnews wp-post">
+			</ul>
+		</div>
+		<div>
+			<a href="./news">
+				<img src="./images/b_watchmore.png" alt="WATCH MORE" width="88" class="pc">
+				<img src="./images/b_watchmore_sp.png" alt="WATCH MORE" width="180" class="sp-inline">
+			</a>
+		</div>
+	</div>
+</section><!--/top_news-->
+
+<!--診療時間-->
+<section class="top_info">
+	<div class="info">
+		<div>
+			<h2 class="t_top"><img src="./images/t_info.png" alt="診療時間" width="126"></h2>
+		</div>
+		<div>
+			<img src="./images/tel.png" alt="0565-34-1155" width="147" class="tel_img">
+		</div>
+		<div>
+			<table class="table_info">
+				<thead>
+				<tr>
+					<td>診療時間</td>
+					<td>月</td>
+					<td>火</td>
+					<td>水</td>
+					<td>木</td>
+					<td>金</td>
+					<td>土</td>
+				</tr>
+				</thead>
+				<tbody>
+				<tr>
+					<td>9:30〜12:30</td>
+					<td>○</td>
+					<td>○</td>
+					<td>○</td>
+					<td>○</td>
+					<td>○</td>
+					<td>○</td>
+				</tr>
+				<tr>
+					<td>15:30〜18:30</td>
+					<td>○</td>
+					<td>−</td>
+					<td>○</td>
+					<td>○</td>
+					<td>○</td>
+					<td>−</td>
+				</tr>
+				</tbody>
+			</table>
+			<p class="shinryo_mes">休診日：火曜午後・土曜午後・日曜日・祝日<br>疾患の内容により、すべての検査が行えない場合があります。</p>
+			<a href="./rsvapp" class="b_yoyaku">診療の予約をする</a>
+		</div>
+	</div>
+</section><!--/top_info-->
+
+<section class="top_access">
+	<div class="top_access_box">
+		<div>
+			<h2 class="t_top t_access"><img src="./images/t_access.png" alt="アクセス" width="126"></h2>
+			<address class="access_adrs">[深見眼科]<br>〒471-0079<br>豊田市陣中町1-6-11</address>
+			<div class="access_text">
+				【電車をご利用のお客様】<br>名鉄梅坪駅下車、高架沿いに豊田方面へ(徒歩7〜8分)<br>
+				【お車をご利用のお客様】<br>深見眼科ビル前に専用平面駐車20台以上有り
+			</div>
+		</div>
+		<div>
+			<div class="map">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d6528.839342658499!2d137.15834090270204!3d35.0962301656218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1z5rex6KaL55y856eR!5e0!3m2!1sja!2sjp!4v1555767528457!5m2!1sja!2sjp" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="top_banner">
+		<h2 class="t_small m-t-40">コンタクトレンズのご用命は</h2>
+		<div class="al-c"><a href="http://boston-club.jp/" target="_blank"><img src="./images/logo_fukamicontact.png" alt="Fukami Contact"></a></div>
+		<div class="al-c m-t-20"><a href="./recruit.html"><img src="./images/info_recruit.png" alt="採用情報"></a></div>
+</section><!--/top_banner-->
+
+</div><!--/top_contents-->
+
+<footer class="footer clearfix">
+	<a href="./index.html" class="f_logo"><img src="./images/logo.svg" alt="深見眼科"></a>
+	<div class="copyright"><i class="icon-at"></i> Fukami eye clinic All Right Reserved.</div>
+</footer>
+
+<div class="pagetop"><div class="inner"><i class="icon-up-open-big"></i></div></div>
+
+</div><!--/page-->
+
+<script src="./js/slick/slick.min.js"></script>
+<script src="./js/jquery.easing.1.3.js"></script>
+<script src="./js/script.js?<?= time()?>"></script>
+
+<script>
+$.getJSON( "https://fukami-ganka.jp/news/wp-json/wp/v2/posts", function(results) {
+      $.each(results, function(i, item) {
+		console.log(i);
+		if(i === 4){
+			return false;
+		}
+        var date_data = new Date(item.date);
+        var year = date_data.getFullYear();
+        var month = date_data.getMonth() + 1;
+        var day = date_data.getDate();
+        var title = item.title.rendered;
+        var link = item.link;
+        var date = year + '.' + month + '.' + day;
+		
+        $('.wp-post').append("<li><a href='" + link + "'><span>"+ date + "</span>" + title + "</a></li>");
+      });
+    });
+</script>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,173 @@
+//ua
+var ua = navigator.userAgent;
+var ua_flag;
+if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || (ua.indexOf('Android') > 0) && (ua.indexOf('Mobile') > 0) || ua.indexOf('Windows Phone') > 0) {
+	ua_flag = 'sp';
+	ua_sp();
+}else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+	ua_flag = 'mobile';
+}else{
+	ua_flag = 'pc';
+	$('body').addClass('hover');
+}
+
+//pagetop
+var pagetop = $('.pagetop');
+$(window).scroll(function(){
+	if($(this).scrollTop() > 150){
+		pagetop.fadeIn(400);
+	}else{
+		pagetop.fadeOut(400);
+		setTimeout(function(){
+			pagetop.removeClass('active');
+		},400);
+	}
+});
+pagetop.click(function(){
+	pagetop.addClass('active');
+	$('body,html').animate({
+		scrollTop: 0
+	}, 800 ,'easeInOutCirc');
+	return false;
+});
+
+//Insert
+$('.nav_list a').prepend('<i class="icon-right-open-big"></i>');
+
+//メイン画像
+$(window).on('load',function(){
+$('.slider').slick({
+	fade:true,
+	autoplay:true,
+	arrows:true,
+	autoplaySpeed:6000,
+	speed:1500,
+	pauseOnFocus:false,
+	pauseOnHover:false,
+	pauseOnDotsHover:false,
+	prevArrow:'.top-prev',
+	nextArrow:'.top-next',
+	swipe:true,
+	cssEase:'ease-out',
+	useCSS:true,
+	dots:false,
+	centerPadding:0
+});
+
+$('.mi1').removeClass('slick-current').addClass('my');
+setTimeout(function(){
+	$('.mi1.my').addClass('slick-current').removeClass('my');
+},100);
+
+$('.main').addClass('on');
+});
+
+$(document).on('touchend click','.nav-button-wrap',function(event){
+	eventHandler(event,$(this));
+	$('.nav_wrap').slideToggle(300);
+	$('.main_nav').toggleClass('active');
+	$('.nav-button').toggleClass('active');
+	$(this).toggleClass('active');
+});
+
+if($('.block_yoyaku').length){
+$('.b_yoyaku_start').matchHeight();
+}
+
+
+$(window).on("load resize orientationchange",function(){
+	responsive();
+	//main_resize();
+	//w_test();
+});
+
+function responsive(){
+var w = window.innerWidth;
+var h = $(window).height();
+
+var s = 400;
+var x = 767;
+var y = 1024;
+if (w <= s){
+//sp1
+
+}
+else if (w > s && w <= x){
+//sp2
+
+}
+else if (w > x && w <= y){
+//tb
+
+}
+else{
+/*pc*/
+
+}
+
+}
+responsive();
+
+function eventHandler(event,selector){
+	event.stopPropagation();
+	event.preventDefault();
+	if(event.type === 'touchend')selector.off('click');
+}
+
+//smart phone
+function ua_sp(){
+	$('.tel').each(function(){
+		var telnum = $(this).text();
+		$(this).html(
+			telnum.replace(telnum,'<a href="tel:' + telnum + '">' + telnum + '</a>')
+		);
+	});
+	$('.tel_img').each(function(){
+		var telnum = $(this).attr('alt');
+		$(this).wrap('<a href="tel:'+ telnum +'"></a>');
+	});
+}
+
+//test
+function w_test(){
+	var test = window.innerWidth;
+	$('title').text(test);
+}
+
+//商品スクロール
+$('.pn_box').on('click',function(){
+	var i = $('.pn_box').index(this);
+	var p = $('.t_product').eq(i).offset().top;
+	var space = 0;
+	if (window.innerWidth <= 768){
+		space = 20;
+	}else{
+		space = 10;
+	}
+	$('html,body').stop().animate({ scrollTop: p - space}, 1000, 'easeOutCubic');
+	return false;
+});
+
+//ヘッダー固定
+$('.nav .nav_list').clone().appendTo('.sub_nav');
+$(window).on('load scroll touchmove resize',function(){
+	var w = $(window).innerWidth();
+	var elm = $('.sub_header');
+	var elm_pos = $('.fix_pos');
+	if(elm_pos.length && w > 767){
+		var fix_pos = elm_pos.offset().top;
+		if($(this).scrollTop() > fix_pos){
+			elm.addClass('on');
+		}else{
+			elm.removeClass('on');
+		}
+	}
+});
+
+function main_resize(){
+	var w = $(window).innerWidth();
+	var h = w * (618 / 1000);
+	if(w <= 1000){
+		$('.main,.mi').css('height',h+'px');
+	}
+}
